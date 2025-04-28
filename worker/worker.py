89752fdb08b03fa16e90f63caebd2de69707a8be
Fenil Jikadara <feniljikadara2004@gmail.com>
2025-04-29 01:12:10 +0530

    Switch Celery broker to RabbitMQ (CloudAMQP)

--- worker/worker.py
+++ worker/worker.py
@@ -1,12 +1,10 @@
 from celery import Celery
 import os
-from dotenv import load_dotenv
-load_dotenv()
 
 celery_app = Celery(
     "worker",
-    broker=os.getenv("REDIS_BROKER_URL"),
-    backend=os.getenv("REDIS_BROKER_URL"),
+    broker=os.getenv("BROKER_URL"),  
+    backend=os.getenv("REDIS_CACHE_URL"), 
 )
 
 celery_app.conf.task_routes = {
