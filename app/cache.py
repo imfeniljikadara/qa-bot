b5d1d472db5df415cfe623b0f9e11f5300841ddf
Fenil Jikadara <feniljikadara2004@gmail.com>
2025-04-29 01:22:54 +0530

    Final integration with RabbitMQ broker/backend and Railway Redis cache

--- app/cache.py
+++ app/cache.py
@@ -1,7 +1,7 @@
 import redis.asyncio as redis
 import os
 
-redis_client = redis.from_url(os.getenv("REDIS_CACHE_URL"), decode_responses=True)
+redis_client = redis.from_url(os.getenv("CACHE_URL"), decode_responses=True)
 
 async def cache_get(key: str):
     return await redis_client.get(key)
--- worker/worker.py
+++ worker/worker.py
@@ -3,8 +3,8 @@ import os
 
 celery_app = Celery(
     "worker",
-    broker=os.getenv("BROKER_URL"),  
-    backend=os.getenv("REDIS_CACHE_URL"), 
+    broker=os.getenv("BROKER_URL"),   
+    backend=os.getenv("BROKER_URL"),  
 )
 
 celery_app.conf.task_routes = {
